49cf2b86062215354aff08346de774892f09936d
mcsepro <aremsony2k4@yahoo.com>
2023-01-17 16:24:37 +0100

    102 added

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -21,3 +21,22 @@ if __name__ == "__main__":
     )
 
     print(f'{check} status check')
+    print("IPs:")
+    ips = db_nginx.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    for ip in ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
